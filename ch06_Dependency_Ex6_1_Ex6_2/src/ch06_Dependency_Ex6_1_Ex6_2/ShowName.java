b4bfa3750a736c4d73e5e42a021c35bf14804de0
Techaniti Chantakien <techaniti.chantakien@rail.bombardier.com>
2016-05-03 12:08:08 +0700

    First Commit project ch06_Dependency_Ex6_1_Ex6_2

---
+++ ch06_Dependency_Ex6_1_Ex6_2/src/ch06_Dependency_Ex6_1_Ex6_2/ShowName.java
@@ -0,0 +1,15 @@
+package ch06_Dependency_Ex6_1_Ex6_2;
+
+public class ShowName {
+	public void showMyName(){
+		System.out.println("Mr.Incredible");
+	}
+	
+	public void showMySurname(){
+		System.out.println("Jone");
+	}
+
+	public void showMyAddress(){
+		System.out.println("Thailand.");
+	}
+}
---
+++ ch06_Dependency_Ex6_1_Ex6_2/src/ch06_Dependency_Ex6_1_Ex6_2/ShowNameApp.java
@@ -0,0 +1,14 @@
+package ch06_Dependency_Ex6_1_Ex6_2;
+
+public class ShowNameApp {
+
+	public static void main(String[] args) {
+		ShowName name = new ShowName();
+		
+		name.showMyName();
+		name.showMySurname();
+		name.showMyAddress();
+
+	}
+
+}
